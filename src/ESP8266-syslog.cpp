c50924b563e8361811666211edb99f15002414f7
Jim McGinness <jmcgnh@gmail.com>
2024-02-12 04:01:19 -0500

    Switch to dynamic build_flags

--- src/ESP8266-syslog.cpp
+++ src/ESP8266-syslog.cpp
@@ -9,47 +9,66 @@
 
 const char vfname[] = __FILE__;
 const char vtimestamp[] = __DATE__ " " __TIME__;
-String versionstring = "20230826.0230.1";
+const char projdir[] = PROJECT_DIR;
+const char branch[] = BRANCH;
+const char ltimestamp[] = TIMESTAMP;
+const char commit_hash[] = COMMIT_HASH;
+const char project_url[] = PROJECT_URL;
 String myHostname = "weather26";
 unsigned long needident = 1;
 
 unsigned long currentMillis;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println("Booting");
 
   wifi_setup();
- 
+
   // Identification
   Serial.println();
   Serial.print("file: ");
   Serial.println(vfname);
-  Serial.print("timestamp (local time): ");
-  Serial.println(vtimestamp);
-  Serial.println(versionstring);
-  Serial.println("https://gitea.jmcg.net/jmcg/WeatherStation06");
+  Serial.print("Project dir: ");
+  Serial.println(projdir);
+  Serial.print("current branch: ");
+  Serial.print(branch);
+  Serial.print("  commit: ");
+  Serial.print(commit_hash);
+  Serial.print("   compile timestamp: ");
+  Serial.println(ltimestamp);
+  Serial.print("Project URL: ");
+  Serial.println(project_url);
   Serial.println();
 
   blink_setup();
- }
+}
 
-void loop() {
+void loop()
+{
 
-  if( needident){
-    needident=0;
+  if (needident)
+  {
+    needident = 0;
     // repeat identification occasionally
+    // Identification
     Serial.println();
     Serial.print("file: ");
     Serial.println(vfname);
-    Serial.print("timestamp (local time): ");
-    Serial.println(vtimestamp);
-    Serial.println(versionstring);
-    Serial.println("https://github.com/jmcgnh/ESP8266-syslog");
+    Serial.print("Project dir: ");
+    Serial.println(projdir);
+    Serial.print("current branch: ");
+    Serial.print(branch);
+    Serial.print("  commit: ");
+    Serial.println(commit_hash);
+    Serial.println(ltimestamp);
+    Serial.print("Project URL: ");
+    Serial.println(project_url);
     Serial.println();
   }
-  
+
   currentMillis = millis();
   wifi_handler();
   blink_handler();
