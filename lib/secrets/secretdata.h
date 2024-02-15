54a29c24ad3228f1d48acc983653eb13c8255e37
Jim McGinness <jmcgnh@gmail.com>
2024-02-15 00:13:19 -0500

    Move syslog_secrets to untracked syslog_secrets.sh - add to build_flags

--- lib/secrets/secretdata.h
+++ lib/secrets/secretdata.h
@@ -3,6 +3,3 @@
  extern std::map<const char *, const char *> wifisecret;
 
  extern std::map<const char *, const char *> phantsecret;
-
-#define SYSLOG_HOST "syslog5514.jmcg.net"
-#define SYSLOG_PORT 5514
